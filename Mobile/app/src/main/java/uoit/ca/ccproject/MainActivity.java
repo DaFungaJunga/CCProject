7ea1a129dbf8d1d6076cb99449f38bfcf5d3399c
Justin Mendes <100550776@oncampus.local>
2019-03-18 12:04:47 -0400

    no change
    -needed to push to pull

--- Mobile/app/src/main/java/uoit/ca/ccproject/MainActivity.java
+++ Mobile/app/src/main/java/uoit/ca/ccproject/MainActivity.java
@@ -31,6 +31,9 @@ public class MainActivity extends Activity {
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
+
+
+
             try{
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
